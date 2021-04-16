166a6e88b9191757f769ce97c36b0921f5aea9c8
niehausandrew <43279896+niehausandrew@users.noreply.github.com>
2021-04-16 18:34:20 -0500

    Made modular based on user input

--- numbers.js
+++ numbers.js
@@ -1,16 +1,20 @@
-function loadValues(n, elem){
+function loadValues(n, elem, modThreeValue, modFiveValue){
     var values = [];
     for (var i = 1; i <= n; i++){
-        var translated = translate(i);
+        var translated = translate(i, modThreeValue, modFiveValue);
         values.push('<tr><td>' + i + '</td><td>' + translated + '</td></tr>');
     }
     elem.innerHTML = values.join('');
 }
 
-function translate(value){
+/*
+    Takes in an integer value and outputs the corresponding
+    value depending on what divides the input
+*/
+function translate(value, modThreeValue, modFiveValue){
     var output = '';
-    if (value % 3 == 0) output += "SCHEELS";
-    if (value % 5 == 0) output += ".COM";
+    if (value % 3 == 0) output += modThreeValue;
+    if (value % 5 == 0) output += modFiveValue;
     if (output == '') return value;
     return output;
 }
