1c15978a18c4fb5e3684231a2b3f8358759709d5
Andrew Niehaus <andrew.niehaus@ndsu.edu>
2021-04-19 15:57:39 -0500

    Added validation and min value input

--- numbers.js
+++ numbers.js
@@ -1,20 +1,25 @@
-function loadValues(n, elem, modThreeValue, modFiveValue){
-    var values = [];
-    for (var i = 1; i <= n; i++){
-        var translated = translate(i, modThreeValue, modFiveValue);
-        values.push('<tr><td>' + i + '</td><td>' + translated + '</td></tr>');
-    }
-    elem.innerHTML = values.join('');
+/*
+    Main function for adding rows to the data table
+*/
+function loadValues(min, max, table, modThreeValue, modFiveValue) {
+  min = parseInt(min);
+  max = parseInt(max);
+  var values = [];
+  for (var i = min; i <= max; i++) {
+    var translated = translate(i, modThreeValue, modFiveValue);
+    values.push("<tr><td>" + i + "</td><td>" + translated + "</td></tr>");
+  }
+  table.innerHTML = values.join("");
 }
 
 /*
     Takes in an integer value and outputs the corresponding
     value depending on what divides the input
 */
-function translate(value, modThreeValue, modFiveValue){
-    var output = '';
-    if (value % 3 == 0) output += modThreeValue;
-    if (value % 5 == 0) output += modFiveValue;
-    if (output == '') return value;
-    return output;
+function translate(value, modThreeValue, modFiveValue) {
+  var output = "";
+  if (value % 3 == 0) output += modThreeValue;
+  if (value % 5 == 0) output += modFiveValue;
+  if (output == "") return value;
+  return output;
 }
